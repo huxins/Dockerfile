797a8cd93b67733d7b8b08c2bb6bd751f08bf615
“dhuxins <huxins@163.com>
2020-08-31 21:09:20 +0800

    add open-vm-tools

---
+++ Docker-xpenology-open-vm-tools/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ -e /root/.ssh/open-vm-tools ] || ssh-keygen -t rsa -f /root/.ssh/open-vm-tools -q -P ""
+[ -e /root/.ssh/authorized_keys ] || touch /root/.ssh/authorized_keys
+
+PUBLIC_KEY=$(cat /root/.ssh/open-vm-tools.pub)
+grep -q -F "$PUBLIC_KEY" /root/.ssh/authorized_keys || echo $PUBLIC_KEY >> /root/.ssh/authorized_keys
+
+exec /usr/bin/vmtoolsd
+
